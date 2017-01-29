233f3d3cf9b445ec4d114c3142d57d2a9835cb4d
Pavel <trusiwka@gmail.com>
2017-01-29 20:53:27 +0400

    1

---
+++ installer.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    //"os"
+	"os/exec"
+	"io"
+	"fmt"
+	"log"
+	"bufio"
+)
+
+func checkError(err error) {
+    if err != nil {
+        log.Fatalf("Error: %s", err)
+    }
+}
+
+func read(reader io.ReadCloser) {
+	nBytes, nChunks := int64(0), int64(0)
+    r := bufio.NewReader(reader)
+    buf := make([]byte, 0, 4*1024)
+    for {
+        n, err := r.Read(buf[:cap(buf)])
+        buf = buf[:n]
+        if n == 0 {
+            if err == nil {
+                continue
+            }
+            if err == io.EOF {
+                break
+            }
+            log.Fatal(err)
+        }
+        nChunks++
+        nBytes += int64(len(buf))
+        // process buf
+        if err != nil && err != io.EOF {
+            log.Fatal(err)
+        }
+		fmt.Println(">>>", buf, nBytes, nChunks)
+    }
+	fmt.Println("end")
+}
+
+func main() {
+    cmd := exec.Command("sqlplus", "/nolog")
+
+    // Create stdout, stderr streams of type io.Reader
+    stdout, err := cmd.StdoutPipe()
+    checkError(err)
+    //stderr, err := cmd.StderrPipe()
+    //checkError(err)
+
+    // Start command
+    err = cmd.Start()
+    checkError(err)
+
+    // Don't let main() exit before our command has finished running
+    defer cmd.Wait()  // Doesn't block
+
+    // Non-blockingly echo command output to terminal
+	
+	read(stdout)
+    // io.Copy(os.Stdout, stdout)
+    // io.Copy(os.Stderr, stderr)
+
+    // I love Go's trivial concurrency :-D
+    fmt.Printf("Do other stuff here! No need to wait.\n\n")
+}
